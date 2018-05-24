5cd92c428af7ca2cbd4684e2d9a4c46984dbc809
JaehoonShin <39240681+JaehoonShin@users.noreply.github.com>
2018-05-24 17:28:54 +0900

    Add files via upload

--- redistribute.m
+++ redistribute.m
@@ -1,56 +1,20 @@
-function [newtestset] = redistribute(trainX,trainLabel)
+function [newtestset] = redistribute(trainLabel)
 
 % Element per Label
 EpL = 200;
 
 newtestset = zeros(1, EpL * 10);
 
-index1 = [];
-index2 = [];
-index3 = [];
-index4 = [];
-index5 = [];
-index6 = [];
-index7 = [];
-index8 = [];
-index9 = [];
-index10 = [];
+[~,sumIndex] = sort(trainLabel,'ascend');
+indexNum=zeros(10,1);
 
-for i = 1:length(trainLabel)
-    switch trainLabel(i)
-        case 10
-            index10 = [i;index10];
-        case 1
-            index1 = [i;index1];
-        case 2
-            index2 = [i;index2];
-        case 3
-            index3 = [i;index3];
-        case 4
-            index4 = [i;index4];
-        case 5
-            index5 = [i;index5];    
-        case 6
-            index6 = [i;index6];
-        case 7
-            index7 = [i;index7];
-        case 8
-            index8 = [i;index8];
-        case 9
-            index9 = [i;index9];
-    end
+for ii=1:10
+    indexNum(ii)=sum(length(find(trainLabel==ii)));
 end
-
-
-sumIndex = [index1;index2;index3;index4;index5;index6;index7;index8;index9;index10];
-indexNum = [length(index1);length(index2);length(index3);length(index4);length(index5);length(index6);length(index7);length(index8);length(index9);length(index10);];
-
-for i = 1:10
-    curIndex = 0;
-    for j = 1:EpL
-        newtestset(EpL*(i-1) + j) = sumIndex(curIndex + randi(indexNum(i)));
-    end
-    curIndex = curIndex + indexNum(i);
+curIndex = 0;
+for ii = 1:10
+    newtestset(EpL*(ii-1)+1:EpL*ii) = sumIndex(curIndex + randi(indexNum(ii),1,EpL));
+    curIndex = curIndex + indexNum(ii);
 end
 
 end
